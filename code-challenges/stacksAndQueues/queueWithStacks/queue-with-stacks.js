cdab1ac59a649debb3d772f9e2397869b09e9126
LENA EIVY <applena@gmail.com>
2019-01-08 15:05:12 -0800

    code challenge complete and all tests pass

--- code-challenges/stacksAndQueues/queueWithStacks/queue-with-stacks.js
+++ code-challenges/stacksAndQueues/queueWithStacks/queue-with-stacks.js
@@ -5,18 +5,16 @@ const Stack = require('../stacks');
 class PseudoQueue {
 
   constructor(){
-    this.bottom = null;
-    this.stack = new Stack();
+    this.stack1 = new Stack();
+    this.stack2 = new Stack();
   }
 
   enqueue(value){
     if(arguments.length !== 1){throw 'you must enter a value as arguments';}
     
-    this.stack.push(value);
-    if(!this.bottom) {
-      this.bottom = this.stack.top;
-    }else{
-      this.bottom.previous = this.stack.top;
+    this.stack1.push(value);
+    while(this.stack1){
+      this.stack2.push(this.stack1.pop());
     }
 
   }
@@ -25,15 +23,7 @@ class PseudoQueue {
     if(arguments.length){throw 'this does not take an argument';}
     if(!this.bottom){throw 'this is an empty pseudoQ';}
     // it won't have a previous if this is a stack of 1 item
-    if(this.bottom.previous){
-      this.bottom.previous.next = null; // delete knowledge of bottom from stack
-    }
-
-    let temp = this.bottom.value;
-    // move the bottom reference to the new last item in the stack
-    this.bottom = this.bottom.previous;
-
-    return temp;
+    return this.stack2.pop();
   }
 }
 
