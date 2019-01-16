e6c4a8ba329f7df8ab28ee509acf3977e5c1aa33
LENA EIVY <applena@gmail.com>
2019-01-15 21:54:11 -0800

    challenge complete and 2/3 tests pass

--- code-challenges/tree/tree.js
+++ code-challenges/tree/tree.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let Node = require('./Node');
+let Queue = require('../stacksAndQueues/queues');
 
 class BinaryTree {
   constructor () {
@@ -54,22 +54,47 @@ class BinaryTree {
 
   }
 
+  breadthFirst(){
+
+    let nodes = [];
+    let breadth = new Queue();
+
+    breadth.enqueue(this.root);
+    while(breadth.size){
+      // console.log('breadth.front.value', breadth.front.value)
+      nodes.push(breadth.front.value.value);
+
+      if(breadth.front.left){
+        breadth.enqueue(breadth.front.left);
+      }
+
+      if(breadth.front.right){
+        breadth.enqueue(breadth.front.right);
+      }
+
+      breadth.dequeue(breadth.front);
+    }
+
+    return nodes;
+  }
+
 }
 
-let tree = new BinaryTree();
-let a = new Node('a');
-let b = new Node('b');
-let c = new Node('c');
-let d = new Node('d');
-let e = new Node('e');
-let f = new Node('f');
+// let Node = require('./Node');
+// let tree = new BinaryTree();
+// let a = new Node('a');
+// let b = new Node('b');
+// let c = new Node('c');
+// let d = new Node('d');
+// let e = new Node('e');
+// let f = new Node('f');
 
-a.left = b;
-a.right = c;
-c.left = f;
-b.left = d;
-b.right = e;
-tree.root = a;
+// a.left = b;
+// a.right = c;
+// c.left = f;
+// b.left = d;
+// b.right = e;
+// tree.root = a;
 
 module.exports = BinaryTree;
 
