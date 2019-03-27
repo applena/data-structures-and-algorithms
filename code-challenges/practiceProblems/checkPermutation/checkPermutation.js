73efb8a7b88ee305608d736ccb1339cda8aac13f
LENA EIVY <applena@gmail.com>
2019-03-26 17:41:39 -0700

    two more challenges complete

---
+++ code-challenges/practiceProblems/checkPermutation/checkPermutation.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Check Permutation: Given two strings, write a method to decide if one is a permutation of the
+// other. 
+
+function checkPermutation(str1, str2){
+  let str1Arr = str1.split('');
+  console.log(str1Arr);
+}
+
+checkPermutation('hello', 'hi');
